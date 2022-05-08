7848f5be9290dcc15b082dc092c2709e941eb337
Omer Etrog <omer72@gmail.com>
2022-05-08 12:07:21 +0000

    Add Promise

--- primtUsers.js
+++ primtUsers.js
@@ -2,22 +2,23 @@ const users = [
   {name :'a'},{name :'b'},{name :'c'},{name :'d'},{name :'e'}
               ]
 
+   
   function print(t){
-   
-     setTimeout(function(){
-       console.log('User '+t);
-       
-     },4000);
-   
-   
-}
-
- function printUsers(u){
-  for(let user of u){
-       print(user.name);    
-     
-  }
-}
+    let pr = new Promise((resolve) =>{
+      setTimeout(function(){
+        console.log('hello '+t);
+        resolve();
+      },1000);
+    })
+    return pr;
+ }
+ 
+  async function printUsers(u){
+   for(let user of u){
+        await print(user.name);    
+      
+   }
+ }
 
 
 printUsers(users);
